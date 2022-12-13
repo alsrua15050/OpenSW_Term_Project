c4b1141fde5770c7403a9ea08890282ad766a7d7
Mingyeom <alsrua15050@naver.com>
2022-12-13 22:06:52 +0900

    add track face line(Through chin and eyebrows)

--- face_saver.py
+++ face_saver.py
@@ -10,7 +10,7 @@ cascade_file = 'haarcascade_frontalface_alt.xml'
 cascade = cv2.CascadeClassifier(cascade_file)
 
 # Reading the image with dlib
-image_file = 'gd.jpeg'
+image_file = 'test_img.jpeg'
 img = cv2.imread(image_file)
 
 # Face detection
@@ -27,7 +27,7 @@ for (x, y, w, h) in face_list:
     color = (0, 0, 225) 
     pen_w = 2 
     print(x, y, w, h)
-    cv2.rectangle(img, (x, y), (x+w, y+h), color, thickness = pen_w)
+    # cv2.rectangle(img, (x, y), (x+w, y+h), color, thickness = pen_w)
     
 # -------
 
@@ -79,18 +79,42 @@ for i in range(0, len(routes)-1):
    to_coordinate = routes[i+1]
    img = cv2.line(img, from_coordinate, to_coordinate, (255, 255, 0), 1)
 
+# ---------------- 다른 루트
+routes1 = []
+
+for p in range(42, 26, -1):
+    from_coordinate = landmark_tuple[p + 1]
+    to_coordinate = landmark_tuple[p]
+    routes1.append(from_coordinate)
+
+from_coordinate = landmark_tuple[27]
+to_coordinate = landmark_tuple[44]
+routes1.append(from_coordinate)
+
+for p in range(44, 46):
+    from_coordinate = landmark_tuple[p]
+    to_coordinate = landmark_tuple[p + 1]
+    routes1.append(from_coordinate)
+
+from_coordinate = landmark_tuple[46]
+to_coordinate = landmark_tuple[51]
+routes1.append(from_coordinate)
+
+for p in range(51, 53):
+    from_coordinate = landmark_tuple[p]
+    to_coordinate = landmark_tuple[p + 1]
+    routes1.append(from_coordinate)
+
+from_coordinate = landmark_tuple[53]
+to_coordinate = landmark_tuple[43]
+routes1.append(from_coordinate)
+routes1.append(to_coordinate)
+
+# Route connecting
+
+for p in range(0, len(routes1) - 1):
+    from_coordinate = routes1[p]
+    to_coordinate = routes1[p + 1]
+    img = cv2.line(img, from_coordinate, to_coordinate, (255, 255, 0), 1)
+
 # Extract Facial area
-
-mask = np.zeros((img.shape[0], img.shape[1]))
-mask = cv2.fillConvexPoly(mask, np.array(routes), 1)
-mask = mask.astype(np.bool_)
-
-out = np.zeros_like(img)
-out[mask] = img[mask]
-
-
-# out --> 얼굴 형태 따놓은 상태
-# out 좌표 움직여서 얼굴 변경
-
-cv2.imshow('hi', out)
-cv2.waitKey(0)
